f4d0bbd6d246ea4e1793a3516220bef8ec71ef92
Piyush Maurya <piyush.maurya@live.in>
2016-04-04 16:10:34 +0530

    courier added

--- ecom/products/views.py
+++ ecom/products/views.py
@@ -9,15 +9,21 @@ class ProductListView(ListView):
     model = Product
     queryset = Product.objects.all()
 
-    def get_queryset(self, *args, **kwargs):
-        qs = super(ProductListView, self).get_queryset(*args, **kwargs)
-        query = self.request.GET.get("q")
-        if query:
-            qs = self.model.objects.filter(
-                Q(title__contains=query) |
-                Q(description__contains=query)
-            )
-            return qs
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     context["query"] = self.request.GET.get("q")
+    #     return context
+
+
+    # def get_queryset(self, *args, **kwargs):
+    #     qs = super(ProductListView, self).get_queryset(*args, **kwargs)
+    #     query = self.request.GET.get("q")
+    #     if query:
+    #         qs = self.model.objects.filter(
+    #             Q(title__contains=query) |
+    #             Q(description__contains=query)
+    #         )
+    #         return qs
 
 
 class ProductDetailView(DetailView):
